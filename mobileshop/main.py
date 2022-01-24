31dd30aa4dbf8e6d62d39f84284ba72213ec9d7b
Jeevan-Rai <78399976+Jeevan-Rai@users.noreply.github.com>
2022-01-24 23:51:17 +0530

    Update main.py

--- mobileshop/main.py
+++ mobileshop/main.py
@@ -21,3 +21,45 @@ db = SQLAlchemy(app)
 def home():
     prdts = db.session.query(Products.P_NAME,Products.COUNT,Products.COST,Products.P_IMG,Products.P_DESC).all()
     return render_template('c_index.html',products=prdts)
+
+app.route("/login", methods=["GET", "POST"])
+def login():
+    if(request.method == "POST"):
+        uname = request.form.get("uname")
+        psw = request.form.get("psw")
+        exists = db.session.query(Customer.USERNAME).filter_by(
+            USERNAME=uname, PASSWORD=psw).first() is not None
+        if(exists):
+            params["crnt_usr"] = uname
+            session['cust_login'] = True
+            return redirect('/')
+        else:
+            return redirect('/signup')
+    return render_template('login.html')
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if(request.method == "POST"):
+        try:
+            fname = request.form.get("Fname")
+            lname = request.form.get("Lname")
+            email = request.form.get("email")
+            phno = request.form.get("phno")
+            gender = request.form.get("gender")
+            address = request.form.get("address")
+            uname = request.form.get("username")
+            psw = request.form.get("psw")
+            entry = Customer(F_NAME=fname, L_NAME=lname, ADDRESS=address, PH_NO=phno,
+                             E_MAIL=email, GENDER=gender, USERNAME=uname, PASSWORD=psw)
+            db.session.add(entry)
+            data = db.session.query(Customer.CUST_ID).filter_by(
+                USERNAME=uname).first()
+            db.session.flush()
+            entry1 = Cart(CUST_ID=data[0])
+            db.session.add(entry1)
+            db.session.commit()
+            return redirect("/")
+        except:
+            print
+            redirect("/signup")
+    return render_template('signup.html')
